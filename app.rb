231f9fdcce1538dbbcc2fd20797b80c6eb5de744
molodtsovaav <molodtsovaav@gmail.com>
2019-08-19 14:22:22 +0400

    Add opportunity to choose city

--- app.rb
+++ app.rb
@@ -7,40 +7,43 @@ require 'uri'
 require 'rexml/document'
 require 'sinatra/activerecord'
 
-set :database_file, "config/database.yml"
-class City < ActiveRecord::Base
-end
-
-before do
-  @cities = City.all
-end
+CITIES = {'Тольятти' => '86', 'Москва' => '37', 'Санкт-Петербург' => '69', 'Нижний Новгород' => '120', 'Мурманск' => '113', 'Адлер' => '2879'}.freeze
 
 get '/' do
-CLOUDINESS = {0 => 'Ясно', 1 => 'Малооблачно', 2 => 'Облачно', 3 => 'Пасмурно'}
-
-uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/86.xml")
-
-response = Net::HTTP.get_response(uri)
-
-doc = REXML::Document.new(response.body)
-
-
-@city_name = URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
-
-@current_forcast = doc.root.elements['REPORT/TOWN'].elements.to_a[0]
-
-@min_temp = @current_forcast.elements['TEMPERATURE'].attributes['min']
-@max_temp = @current_forcast.elements['TEMPERATURE'].attributes['max']
-
-@max_wind = @current_forcast.elements['WIND'].attributes['max']
-
-@clouds_index = @current_forcast.elements['PHENOMENA'].attributes['cloudiness'].to_i
-@clouds = CLOUDINESS[@clouds_index]
-
-
-
-  erb :index
-
+  @cities = CITIES
+  erb :city
 end
+
+post '/city' do
+  CLOUDINESS = {0 => 'Ясно', 1 => 'Малооблачно', 2 => 'Облачно', 3 => 'Пасмурно'}
+
+  @city = params[:city]
+  if @city.length <= 0
+      @error = 'Введите ваш город'
+    return erb :city
+  end
+
+  @x = CITIES[@city]
+  uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/#{@city}.xml")
+
+  response = Net::HTTP.get_response(uri)
+
+  doc = REXML::Document.new(response.body)
+
+
+  @city_name = URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
+
+  @current_forcast = doc.root.elements['REPORT/TOWN'].elements.to_a[0]
+
+  @min_temp = @current_forcast.elements['TEMPERATURE'].attributes['min']
+  @max_temp = @current_forcast.elements['TEMPERATURE'].attributes['max']
+
+  @max_wind = @current_forcast.elements['WIND'].attributes['max']
+
+  @clouds_index = @current_forcast.elements['PHENOMENA'].attributes['cloudiness'].to_i
+  @clouds = CLOUDINESS[@clouds_index]
+  erb :index
+end
+
 
 
